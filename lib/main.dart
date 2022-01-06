a3768432ec8dcf89ad4f90f9af395aa2c3941e58
shvn-iwa <73055963+shvn-iwa@users.noreply.github.com>
2022-01-06 22:18:55 +0900

    Delete FloatingActionButton.

--- lib/main.dart
+++ lib/main.dart
@@ -37,15 +37,6 @@ const MaterialColor customSwatch = MaterialColor(
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -110,11 +101,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
