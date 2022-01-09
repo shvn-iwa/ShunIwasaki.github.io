eda8668ff3ca348dabe0b268884b9a06df7809d7
shvn-iwa <73055963+shvn-iwa@users.noreply.github.com>
2022-01-09 23:57:25 +0900

    set color theme in themes.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -18,22 +18,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const MaterialColor customSwatch = MaterialColor(
-  0xFF23580C,
-  <int, Color>{
-    50: Color(0xFFE5EBE2),
-    100: Color(0xFFBDCDB6),
-    200: Color(0xFF91AC86),
-    300: Color(0xFF658A55),
-    400: Color(0xFF447130),
-    500: Color(0xFF23580C),
-    600: Color(0xFF1F500A),
-    700: Color(0xFF1A4708),
-    800: Color(0xFF153D06),
-    900: Color(0xFF0C2D03),
-  },
-);
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
