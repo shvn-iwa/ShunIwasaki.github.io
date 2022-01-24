47019e3cf86e6faba5b707ba551a932873afa8bd
shvn-iwa <73055963+shvn-iwa@users.noreply.github.com>
2022-01-24 23:47:58 +0900

    delete class.

--- lib/main.dart
+++ lib/main.dart
@@ -21,16 +21,6 @@ class MySelfIntro extends StatelessWidget {
     );
   }
 
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
