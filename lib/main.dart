204d9c142bdaf1821a0df19b8d12c4f98393bf70
shvn-iwa <73055963+shvn-iwa@users.noreply.github.com>
2022-02-14 23:56:11 +0900

    rewrite color themes.

--- lib/main.dart
+++ lib/main.dart
@@ -19,3 +19,34 @@ class MySelfIntro extends StatelessWidget {
     );
   }
 
+  MaterialColor customSwatch = MaterialColor(
+    0xFF23580C,
+    <int, Color>{
+      50: Color(0xFFE5EBE2),
+      100: Color(0xFFBDCDB6),
+      200: Color(0xFF91AC86),
+      300: Color(0xFF658A55),
+      400: Color(0xFF447130),
+      500: Color(0xFF23580C),
+      600: Color(0xFF1F500A),
+      700: Color(0xFF1A4708),
+      800: Color(0xFF153D06),
+      900: Color(0xFF0C2D03),
+    },
+  );
+
+  MaterialColor customSwatchDark = MaterialColor(
+    0xFF0B5394,
+    <int, Color>{
+      50: Color(0xFFE2EAF2),
+      100: Color(0xFFB6CBDF),
+      200: Color(0xFF85A9CA),
+      300: Color(0xFF5487B4),
+      400: Color(0xFF306DA4),
+      500: Color(0xFF0B5394),
+      600: Color(0xFF0A4C8C),
+      700: Color(0xFF084281),
+      800: Color(0xFF063977),
+      900: Color(0xFF032965),
+    },
+  );
